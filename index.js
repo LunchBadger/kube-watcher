794e343ad802eb3a6a33526092c78ff55579a666
Serhii Kuts <sergeykuc@gmail.com>
2018-10-09 19:34:03 +0300

    added all pieces before building correct state

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const debugEvents = require('debug')('kube-watcher:events');
 const FULL_DUMP_INTERVAL = process.env.FULL_DUMP_INTERVAL || 60000;
 const PODS_URL = process.env.PODS_URL || 'http://localhost:8001' +
   '/api/v1/namespaces/customer/pods';
+const ENDPOINTS_URL = process.env.ENDPOINTS_URL || 'http://localhost:8001' +
+  '/api/v1/namespaces/customer/endpoints';
 const request = require('superagent');
 const channels = {}; // TODO handle remove of user\env
 const pods$ = require('kube-observable')(PODS_URL + '?watch=true');
+const eps$ = require('kube-observable')(ENDPOINTS_URL + '?watch=true');
 
 let data = {};
 
+function rebuildPodState(pod) {
+  const kubeStatus = pod.status;
+  const {
+    instanceType,
+    user,
+    envType
+  } = ensureState(pod.metadata);
+  const status = computeStatus(kubeStatus);
+  const id = pod.metadata.labels.id || 'default';
+  data[user][envType][instanceType][id] = data[user][envType][instanceType][id] || {
+    pods: {}
+  };
+  data[user][envType][instanceType][id].pods[pod.metadata.name] = {
+    status
+  };
+}
 pods$.subscribe(obj => {
   // gateway-demo-dev-gateway-696bb497cd-s7b6p
   try {
@@ -17,12 +36,14 @@ pods$.subscribe(obj => {
     if (!state) {
       return;
     }
-    const {instanceType, user, envType} = state;
-    const kubeStatus = obj.object.status;
+    const {
+      instanceType,
+      user,
+      envType
+    } = state;
     if (obj.type === 'ADDED' || obj.type === 'MODIFIED') {
-      const status = computeStatus(kubeStatus);
-      data[user][envType][instanceType][obj.object.metadata.name] = { status, id: obj.object.metadata.labels.id || null };
-      debugEvents(obj.type, obj.object.metadata.name, status);
+      rebuildPodState(obj.object);
+      debugEvents(obj.type, obj.object.metadata.name);
     } else {
       // for some reason may not happen for gateway, rely on MODIFIED
       delete data[user][envType][instanceType][obj.object.metadata.name];
@@ -34,6 +55,11 @@ pods$.subscribe(obj => {
   }
 });
 
+eps$.subscribe(obj => {
+  // gateway-demo-dev-gateway-696bb497cd-s7b6p
+  console.log(JSON.stringify(obj.object, null, 2));
+});
+
 setInterval(() => {
   // complete reset of internal state.
   // This is becuase watch stream can potentially break and events will be missed
@@ -42,9 +68,16 @@ setInterval(() => {
       data = {};
       const list = res.body;
       for (const pod of list.items) {
-        const {instanceType, user, envType} = ensureState(pod.metadata);
+        const {
+          instanceType,
+          user,
+          envType
+        } = ensureState(pod.metadata);
         const status = computeStatus(pod.status);
-        data[user][envType][instanceType][pod.metadata.name] = { status, id: pod.metadata.labels.id || null };
+        data[user][envType][instanceType][pod.metadata.name] = {
+          status,
+          id: pod.metadata.labels.id || null
+        };
       }
       debug('Full reload completed. #of pods', list.items.length);
     })
@@ -68,7 +101,12 @@ setInterval(() => {
         env['sls-api'] = env['sls-api'] || {};
         env['kubeless-fn'] = env['kubeless-fn'] || {};
       }
-      ch.send({ data: JSON.stringify(currentState) });
+      ch.send({
+        data: JSON.stringify(currentState)
+      });
+      if (k === 'sk') {
+        console.log(JSON.stringify(currentState), null, 2);
+      }
     }
   }
 }, 3000);
@@ -94,7 +132,10 @@ http.createServer(function (req, res) {
   console.log('Access SSE stream at http://127.0.0.1:7788/channels/{username}');
 });
 
-function ensureState ({ name, labels }) {
+function ensureState({
+  name,
+  labels
+}) {
   const user = labels.producer;
   if (!user) return null;
   const envType = labels.environment || 'dev';
@@ -111,10 +152,14 @@ function ensureState ({ name, labels }) {
   });
   data[user][envType] = data[user][envType] || {};
   data[user][envType][instanceType] = data[user][envType][instanceType] || {};
-  return {instanceType, user, envType};
+  return {
+    instanceType,
+    user,
+    envType
+  };
 }
 
-function computeStatus (kubeStatus) {
+function computeStatus(kubeStatus) {
   const status = {
     running: false,
     stopped: false,
