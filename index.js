fa28a2987d750b9a15e2cd6e4cc4a78692af1913
Serhii Kuts <sergeykuc@gmail.com>
2018-10-10 17:36:00 +0300

    reversed state to make id as top level not podname

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const channels = {}; // TODO handle remove of user\env
 const pods$ = require('kube-observable')(PODS_URL + '?watch=true');
 const eps$ = require('kube-observable')(ENDPOINTS_URL + '?watch=true');
 
-let data = {};
+const data = {};
 
 function rebuildPodState(pod) {
   const kubeStatus = pod.status;
@@ -46,7 +46,7 @@ pods$.subscribe(obj => {
       debugEvents(obj.type, obj.object.metadata.name);
     } else {
       // for some reason may not happen for gateway, rely on MODIFIED
-      delete data[user][envType][instanceType][obj.object.metadata.name];
+      delete data[user][`${envType}-${instanceType}`][obj.object.metadata.name];
       debugEvents('DELETED', obj.object.metadata.name);
     }
   } catch (err) {
@@ -57,31 +57,31 @@ pods$.subscribe(obj => {
 
 eps$.subscribe(obj => {
   // gateway-demo-dev-gateway-696bb497cd-s7b6p
-  console.log(JSON.stringify(obj.object, null, 2));
+  // console.log(JSON.stringify(obj.object, null, 2));
 });
 
 setInterval(() => {
   // complete reset of internal state.
   // This is becuase watch stream can potentially break and events will be missed
-  request.get(PODS_URL)
-    .then(res => {
-      data = {};
-      const list = res.body;
-      for (const pod of list.items) {
-        const {
-          instanceType,
-          user,
-          envType
-        } = ensureState(pod.metadata);
-        const status = computeStatus(pod.status);
-        data[user][envType][instanceType][pod.metadata.name] = {
-          status,
-          id: pod.metadata.labels.id || null
-        };
-      }
-      debug('Full reload completed. #of pods', list.items.length);
-    })
-    .catch(err => debug(err));
+  // request.get(PODS_URL)
+  //   .then(res => {
+  //     data = {};
+  //     const list = res.body;
+  //     for (const pod of list.items) {
+  //       const {
+  //         instanceType,
+  //         user,
+  //         envType
+  //       } = ensureState(pod.metadata);
+  //       const status = computeStatus(pod.status);
+  //       data[user][envType][instanceType][pod.metadata.name] = {
+  //         status,
+  //         id: pod.metadata.labels.id || null
+  //       };
+  //     }
+  //     debug('Full reload completed. #of pods', list.items.length);
+  //   })
+  //   .catch(err => debug(err));
 }, FULL_DUMP_INTERVAL);
 
 const SseChannel = require('sse-channel');
@@ -93,19 +93,19 @@ setInterval(() => {
     const ch = channels[k];
     if (ch) {
       const currentState = data[k];
-      for (const envName in currentState) {
-        // ensure that env state has all types present for consistency
-        const env = currentState[envName];
-        env.gateway = env.gateway || {};
-        env.workspace = env.workspace || {};
-        env['sls-api'] = env['sls-api'] || {};
-        env['kubeless-fn'] = env['kubeless-fn'] || {};
-      }
+      // for (const envName in currentState) {
+      //   // ensure that env state has all types present for consistency
+      //   const env = currentState[envName];
+      //   env.gateway = env.gateway || {};
+      //   env.workspace = env.workspace || {};
+      //   env['sls-api'] = env['sls-api'] || {};
+      //   env['kubeless-fn'] = env['kubeless-fn'] || {};
+      // }
       ch.send({
         data: JSON.stringify(currentState)
       });
       if (k === 'sk') {
-        console.log(JSON.stringify(currentState), null, 2);
+        console.log(JSON.stringify(currentState, null, 2));
       }
     }
   }
@@ -132,10 +132,7 @@ http.createServer(function (req, res) {
   console.log('Access SSE stream at http://127.0.0.1:7788/channels/{username}');
 });
 
-function ensureState({
-  name,
-  labels
-}) {
+function ensureState({labels}) {
   const user = labels.producer;
   if (!user) return null;
   const envType = labels.environment || 'dev';
